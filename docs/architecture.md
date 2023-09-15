fd7245b76570be15525b47acffe2ac9501c8a533
Arvind Soni <arvindsoni@gmail.com>
2023-09-14 19:40:32 -0700

    added cleanup script

---
+++ docs/architecture.md
@@ -0,0 +1,37 @@
+# Gatecheck Architecture
+Gatecheck enables users to define, check, and enforce policies on ECS tasks. Gatecheck is deployed as a Lambda function. Whenever an ECS task is created, ECS publishes an event to Amazon Eventbridge service. We capture this task creation event and invoke Gatecheck Lambda function. The Gatecheck function then fetches the task details. 
+
+The user-defined task policies are stored in SSM Parameter store. Gatecheck fetches the policies from SSM Parmeter store and evaluates task attributes against these policy statements. If a task attribute has value that is not allowed then it is considered a violation. Gatecheck logs the violation under `WARNING` level in the CloudWatch logs associated with the Lambda function. Gatecheck can also post the violation to webhook such as a Slack webhook.
+
+![Gatecheck overview](./gatecheck_overview.png)
+
+## Gatecheck IAM policies
+Gatecheck reads multiple resources in your AWS account and hence needs read, describe, and list access to variety of resources. 
+
+* CloudWatch logs - This is core requirement for Gatecheck Lambda function to create log group, log stream and put logs. 
+* ECS cluster, service, task, and task definition - to get the details for service and task
+* Networking resource network interfaces, route tables, security groups, and security group rules - to check for public IP, public subnet, and inspect security group rules.
+* SSM parameters - to get the task policy
+
+A sample IAM policy file for the Gatecheck Lambda function is available at [.example_lambda_iam_policy.json](https://github.com/arvindsoni80/gatecheck/blob/main/.example_lambda_iam_policy.json). All resource reading functions are implemented in [get_resource.py](https://github.com/arvindsoni80/gatecheck/blob/main/gatecheck/get_resource.py).
+
+## Deploying Gatecheck with SAM CLI
+[The AWS Serverless Application Model (AWS SAM)](https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/what-is-sam.html) can be used to build and deploy the Gatecheck function. SAM CLI needs two configuration files and we provided example for each of them - [.example_samconfig.toml](https://github.com/arvindsoni80/gatecheck/blob/main/.example_samconfig.toml) and [.example_template.yaml](https://github.com/arvindsoni80/gatecheck/blob/main/.example_template.yaml).
+
+The [quick start](./index.md) provides step by step instructions on how to prepare and deploy Gatecheck in your AWS environment. Below is a summary of those steps to help you adapt for your needs.
+
+* Set the AWS_REGION and AWS_ACCOUNT_ID environment variable. This is crucial as these environment variables are used to adapt provided example files to your environment.
+* Replace region and account ID in the [.example_lambda_iam_policy.json](https://github.com/arvindsoni80/gatecheck/blob/main/.example_lambda_iam_policy.json) file
+* You can use the provided ECS task policy to begin and test [.example_task_policy.json](https://github.com/arvindsoni80/gatecheck/blob/main/.example_task_policy.json)
+* Use the provided [prereq.py](https://github.com/arvindsoni80/gatecheck/blob/main/prereq.py) to create the Gatecheck IAM role and store the task policy in SSM Parameter Store. 
+* Use the same [prereq.py](https://github.com/arvindsoni80/gatecheck/blob/main/prereq.py) to generate the SAM config files. 
+* Then you can use SAM CLI to build and deploy the Gatecheck function. 
+
+
+
+
+
+
+
+
+
--- gatecheck/get_resource.py
+++ gatecheck/get_resource.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 from botocore.config import Config
 import logging
 
@@ -7,56 +8,77 @@ logger = logging.getLogger(__name__)
 def get_ecs_service(region_name, cluster_name, svc_arns):
     services = []
     client = boto3.client("ecs", config=Config(region_name=region_name))
-    response = client.describe_services(
-        cluster = cluster_name,
-        services = svc_arns,
-        include = ['TAGS']
-    )
+    try:
+        response = client.describe_services(
+            cluster = cluster_name,
+            services = svc_arns,
+            include = ['TAGS']
+        )
+    except botocore.exceptions.ClientError as e:
+        logger.error(e)
+        return (services)
     services = response.get("services", [])
     return (services)
 
 def get_ecs_task(region_name, cluster_name, task_arns):
     tasks = []
     client = boto3.client("ecs", config=Config(region_name=region_name))
-    response = client.describe_tasks(
-        cluster = cluster_name,
-        tasks = task_arns,
-        include = ['TAGS']
-    )
+    try:
+        response = client.describe_tasks(
+            cluster = cluster_name,
+            tasks = task_arns,
+            include = ['TAGS']
+        )
+    except botocore.exceptions.ClientError as e:
+        logger.error(e)
+        return (tasks)
     tasks = response.get("tasks", [])
     return (tasks)
 
 def get_ecs_task_definition(region_name, task_def_arn):
     task_definitions = []
     client = boto3.client("ecs", config=Config(region_name=region_name))
-    response = client.describe_task_definition(
-        taskDefinition = task_def_arn,
-        include = ['TAGS']
-    )
+    try:
+        response = client.describe_task_definition(
+            taskDefinition = task_def_arn,
+            include = ['TAGS']
+        )
+    except botocore.exceptions.ClientError as e:
+        logger.error(e)
+        return (task_definitions)
+    
     task_definitions = response.get("taskDefinition", {})
     return (task_definitions)
 
 def get_route_tables(region_name, subnet_id):
     route_tables = []
     client = boto3.client("ec2", config=Config(region_name=region_name))
-    response = client.describe_route_tables(
-        Filters=[
-            {
-                'Name': 'association.subnet-id',
-                'Values': [
-                    subnet_id,
-                ]
-            },
-        ]
-    )
+    try:
+        response = client.describe_route_tables(
+            Filters=[
+                {
+                    'Name': 'association.subnet-id',
+                    'Values': [
+                        subnet_id,
+                    ]
+                },
+            ]
+        )
+    except botocore.exceptions.ClientError as e:
+        logger.error(e)
+        return (route_tables)
     route_tables = response.get("RouteTables", [])
     return (route_tables)
 
 def get_network_interfaces(region_name, network_interface_ids):
     network_interfaces = []
     client = boto3.client("ec2", config=Config(region_name=region_name))
-    response = client.describe_network_interfaces(
-        NetworkInterfaceIds = network_interface_ids
-    )
+    try:
+        response = client.describe_network_interfaces(
+            NetworkInterfaceIds = network_interface_ids
+        )
+    except botocore.exceptions.ClientError as e:
+        logger.error(e)
+        return (network_interfaces)
     network_interfaces = response.get("NetworkInterfaces", [])
     return (network_interfaces)
